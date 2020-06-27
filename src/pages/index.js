65733bb6b894f4e9ddb928dd38cd95132a40f027
christiemegill <christiemegill@gmail.com>
2020-06-26 22:58:52 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import SEO from "../components/seo"
 const IndexPage = () => {
   const headshot = useStaticQuery(graphql`
     query {
-      headshot: file(relativePath: { eq: "IMG_7739.JPG" }) {
+      headshot: file(relativePath: { eq: "ABP_4274 (2).JPG" }) {
         childImageSharp {
           fluid(maxWidth: 1000) {
             ...GatsbyImageSharpFluid
