7e8dce5b5e3584aaf5f8e17535034ec6118f60c1
christiemegill <christiemegill@gmail.com>
2021-12-05 17:41:03 -0800

    Update writing.js

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -9,21 +9,22 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Writing" />
   
-  <Styled.h3> Essays </Styled.h3>
-   <Styled.p>
-        <Styled.a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://medium.com/moms-dont-have-time-to-write/you-are-more-than-the-living-room-furniture-23384cc6c571"
-        >
-  
       <Styled.h3> Essays </Styled.h3>
    <Styled.p>
         <Styled.a
           target="_blank"
           rel="noopener noreferrer"
           href="https://medium.com/moms-dont-have-time-to-write/you-are-more-than-the-living-room-furniture-23384cc6c571"
-        >
+    >
+          You Are More Than the Living Room Furniture
+        </Styled.a>
+      </Styled.p>
+      <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://nightingaleandsparrow.com/woodland/"      
+  >
           You Are More Than the Living Room Furniture
         </Styled.a>
       </Styled.p>
