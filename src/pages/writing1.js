4919fab8d3b80f200e7ac28a24b8607faa66ba24
christiemegill <christiemegill@gmail.com>
2021-12-10 09:45:46 -0800

    Create writing1.js

---
+++ src/pages/writing1.js
@@ -0,0 +1,83 @@
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Styled } from "theme-ui"
+
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Writing" /
+  
+    <Styled.h3> Middle Grade </Styled.h3>
+   <Styled.p>
+  
+      <Styled.h3> Essays </Styled.h3>
+   <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://medium.com/moms-dont-have-time-to-write/you-are-more-than-the-living-room-furniture-23384cc6c571"
+  
+          You Are More Than the Living Room Furniture
+        </Styled.a>
+      </Styled.p>
+      <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://nightingaleandsparrow.com/woodland/"
+        >
+          One Night I Walked in the Woods
+        </Styled.a>
+      </Styled.p>
+      <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://nightingaleandsparrow.com/woodland/"
+        >
+          Books Bind Us
+        </Styled.a>
+      </Styled.p>
+      <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.bonbonbreak.com/letter-to-my-hometown/"
+        >
+          A Letter to My Hometown
+        </Styled.a>
+      </Styled.p>
+      <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.mother.ly/parenting/motherhood-was-the-gateway-to-my-creative-side"
+        >
+          Motherhood Was the Gateway to My Creative Side
+        </Styled.a>
+      </Styled.p>
+      <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.playfullearning.net/resource/raising-compassionate-kids/"
+        >
+          Raising Compassionate Kids
+        </Styled.a>
+      </Styled.p>
+      <Styled.p>
+        <Styled.a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://christiemegill.contently.com/"
+        >
+          More via Contently
+        </Styled.a>
+      </Styled.p>
+    </div>
+  </Layout>
+)
+
+export default SecondPage
